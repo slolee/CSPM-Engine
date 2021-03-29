298f71f33a42fe72879c81ce226763dcdd4d2def
ch4njun <slolee@naver.com>
2021-03-29 21:02:01 +0900

    Initial commit

---
+++ src/test/java/com/ch4njun/cspm/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch4njun.cspm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
