eff6adc452719f9c6199f931194095423a3b0158
ch4njun <slolee@naver.com>
2021-06-14 17:20:26 +0900

    Hot Fix

--- src/main/java/com/ch4njun/cspm/demo/constant/Path.java
+++ src/main/java/com/ch4njun/cspm/demo/constant/Path.java
@@ -1,7 +1,7 @@
 package com.ch4njun.cspm.demo.constant;
 
 public class Path {
-    public final static String PYTHON_PATH = "C:\\Users\\slole\\AppData\\Local\\Programs\\Python\\Python38-32\\python3";
+    public final static String PYTHON_PATH = "C:\\Users\\slole\\AppData\\Local\\Programs\\Python\\Python38-32\\python.exe";
     public final static String ASSESSMENT_SCRIPT_PATH = "src\\main\\resources\\engine\\assessment\\assessment_main.py";
     public final static String RESOURCE_SCRIPT_PATH = "src\\main\\resources\\engine\\resource\\load_resource_main.py";
 }
