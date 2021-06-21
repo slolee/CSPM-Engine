14f864a0a19b3f5cfca61ba2f29740cff14f122e
ch4njun <38906956+slolee@users.noreply.github.com>
2021-06-21 14:04:34 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ pip3 install datetime pytz boto3 pymysql maya
 wget https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz + "/r"
 tar -xzf openjdk-15.0.2_linux-x64_bin.tar.gz
 rm -rf openjdk-15.0.2_linux-x64_bin.tar.gz
-echo 'export JAVA_HOME=/home/ec2-user/jdk-15.0.2' >> ~/.bashrc
+echo 'export JAVA_HOME='$(pwd)'/jdk-15.0.2' >> ~/.bashrc
 source ~/.bashrc
 
 sed -i 's/\[ID\]/'$1'/g' src/main/resources/application.yml
