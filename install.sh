510342f2cea77e0710eb3d4cc3f58cf9f100e42b
ch4njun <38906956+slolee@users.noreply.github.com>
2021-06-28 17:48:47 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ wget https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee
 tar -xzf openjdk-15.0.2_linux-x64_bin.tar.gz
 rm -rf openjdk-15.0.2_linux-x64_bin.tar.gz
 echo 'export JAVA_HOME='$(pwd)'/jdk-15.0.2' >> ~/.bashrc
-source ~/.bashrc
 
 sed -i 's/\[ID\]/'$1'/g' src/main/resources/application.yml
 sed -i 's/\[PW\]/'$2'/g' src/main/resources/application.yml
@@ -14,3 +13,4 @@ sed -i 's/\[PW\]/'$2'/g' src/main/resources/engine/common/db.py
 sed -i 's/\[IP\]/'$3'/g' src/main/resources/engine/common/db.py
 
 sed -i -e 's/\r$//' run.sh
+source ~/.bashrc
